88ed33970a49ac8db75cc60381389e9ab02906c0
z <zafirafedhila@users.noreply.github.com>
2019-08-25 18:39:14 +0700

    Add files via upload

---
+++ format_output.py
@@ -0,0 +1,29 @@
+# Print Biasa
+# print('Hello World')
+
+# Koma
+# thn = 2000
+# print('Umur saya', 2018 - thn)
+
+# Penggabungan
+# nama = 'Mulqan'
+# print('Hello '+nama)
+
+#Format method
+# nama = 'Mulqan'
+# thn = 2000
+# tinggi = 168.5453
+# print('Umur saya {1}, saya {0}, tinggi {2:.1f}'.format(nama, 2018 - thn, tinggi))
+
+# F-String
+# umur = 2018 - thn
+# print(f'Nama saya {nama}, umur {umur}, tinggi {tinggi:.2f}')
+
+
+nama = 'Rydha'
+Pertanyaan = 'Apakah kamu suka ice cream?'
+Jawaban ='Iya'
+print(f' nama {Rydha}, Pertanyaan {Pertanyaan}, Jawaban {Iya}')
+
+
+# fakultas =
